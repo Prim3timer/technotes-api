4c796838882af69d1f3449b201ee8c89cc68bb36
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-11-30 22:57:57 -0500

    allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    // 'https://technotes-zbnt.onrender.com',
+    'https://technotes-zbnt.onrender.com',
     'https://www.technotes-zbnt.onrender.com'
 ]
 
